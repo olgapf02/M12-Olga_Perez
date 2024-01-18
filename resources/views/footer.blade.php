a15deafe0b5e2089eb70159520e1cb63693931bc
olgapf02 <olgapf02@gmail.com>
2024-01-18 17:58:13 +0100

    Footer traducido al catalan completa

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -7,10 +7,10 @@
                             <a href="https://www.dish.co/ES/es/" class="footer-btn">{{ __('reserve')}}</a>
                         </div>
                         <div class="link-item">
-                            <a href="https://mensakas.coopcycle.org/es/" class="footer-btn">{{ __('about us')}}LTL a CASA</a>
+                            <a href="https://mensakas.coopcycle.org/es/" class="footer-btn">{{ __('La Cantina to home')}}</a>
                         </div>
                         <div class="link-item">
-                            <a href="https://mensakas.coopcycle.org/es/" class="footer-btn">{{ __('about us')}}LTL to GO</a>
+                            <a href="https://mensakas.coopcycle.org/es/" class="footer-btn">{{ __('La Cantina to GO')}}</a>
                         </div>
                     </div>
                 </div>
@@ -27,10 +27,10 @@
                         <a target="_blank" href="https://www.instagram.com/cantinalab.coop/"><img src="https://lateral-production.s3.eu-west-3.amazonaws.com/public/icon/instagram.svg" alt="Instagram"></a>
                     </div>
                     <div class="info-restaurante">
-                        <a href="{{ url('/menu') }}" >{{ __('about us')}}Carta</a>
-                        <a href="{{ url('/trabajo') }}" >{{ __('about us')}}Únete a "La Cantina"</a>
-                        <a href="{{ url('/quieres_un_evento') }}">{{ __('about us')}}quieres hacer un evento</a>
-                        <a href="{{ url('/opiniones') }}" >{{ __('about us')}}Tu opinión nos importa</a>
+                        <a href="{{ url('/menu') }}" >{{ __('food card')}}</a>
+                        <a href="{{ url('/trabajo') }}" >{{ __('Join')}}</a>
+                        <a href="{{ url('/quieres_un_evento') }}">{{ __('you want to do an event')}}</a>
+                        <a href="{{ url('/opiniones') }}" >{{ __('your opinion matters to us')}}</a>
                         <h4>937 44 47 99</h4>
                         <a href="{{ url('/trabajo') }}"  target="_blank"  class="footer-btn">{{ __('about us')}}Únete al equipo</a>
                     </div>
