f03071bb9d140be2c14c907f5921467612924547
olgapf02 <olgapf02@gmail.com>
2024-01-18 17:46:19 +0100

    Formularios traducidos al catalan completa

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,13 +4,13 @@
                 <div class="flex items">
                     <div class="footer links">
                         <div class="link-item">
-                            <a href="https://www.dish.co/ES/es/" class="footer-btn">Reservar</a>
+                            <a href="https://www.dish.co/ES/es/" class="footer-btn">{{ __('reserve')}}</a>
                         </div>
                         <div class="link-item">
-                            <a href="https://mensakas.coopcycle.org/es/" class="footer-btn">LTL a CASA</a>
+                            <a href="https://mensakas.coopcycle.org/es/" class="footer-btn">{{ __('about us')}}LTL a CASA</a>
                         </div>
                         <div class="link-item">
-                            <a href="https://mensakas.coopcycle.org/es/" class="footer-btn">LTL to GO</a>
+                            <a href="https://mensakas.coopcycle.org/es/" class="footer-btn">{{ __('about us')}}LTL to GO</a>
                         </div>
                     </div>
                 </div>
@@ -27,12 +27,12 @@
                         <a target="_blank" href="https://www.instagram.com/cantinalab.coop/"><img src="https://lateral-production.s3.eu-west-3.amazonaws.com/public/icon/instagram.svg" alt="Instagram"></a>
                     </div>
                     <div class="info-restaurante">
-                        <a href="{{ url('/menu') }}" >Carta</a>
-                        <a href="{{ url('/trabajo') }}" >Únete a "La Cantina"</a>
-                        <a href="{{ url('/quieres_un_evento') }}">quieres hacer un evento</a>
-                        <a href="{{ url('/opiniones') }}" >Tu opinión nos importa</a>
+                        <a href="{{ url('/menu') }}" >{{ __('about us')}}Carta</a>
+                        <a href="{{ url('/trabajo') }}" >{{ __('about us')}}Únete a "La Cantina"</a>
+                        <a href="{{ url('/quieres_un_evento') }}">{{ __('about us')}}quieres hacer un evento</a>
+                        <a href="{{ url('/opiniones') }}" >{{ __('about us')}}Tu opinión nos importa</a>
                         <h4>937 44 47 99</h4>
-                        <a href="{{ url('/trabajo') }}"  target="_blank"  class="footer-btn">Únete al equipo</a>
+                        <a href="{{ url('/trabajo') }}"  target="_blank"  class="footer-btn">{{ __('about us')}}Únete al equipo</a>
                     </div>
                 </div>
 
