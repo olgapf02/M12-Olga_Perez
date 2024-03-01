a5c4600af6ed74857dff512b8bd793807403f15e
olgapf02 <olgapf02@gmail.com>
2024-03-01 17:51:42 +0100

    layout nuevo funcional y tambien pantalla para logarse tambien  para la vista del admin

---
+++ resources/views/admin_pages/home_adm.blade.php
@@ -0,0 +1,38 @@
+<h2 class="carta-title">{{__('canteen hours')}}</h2>
+<div class="container-horario">
+    <table class="custom-table">
+        <h1></h1>
+        <tr>
+            <th>{{ __('Day') }}</th>
+            <th>{{ __('Hora') }}</th>
+        </tr>
+        <tr>
+            <td>{{ __('Monday') }}</td>
+            <td>12:00 - 00:00</td>
+        </tr>
+        <tr>
+            <td>{{ __('Tuesday') }}</td>
+            <td>12:00 - 18:00</td>
+        </tr>
+        <tr>
+            <td>{{ __('Wednesday') }}</td>
+            <td>12:00 - 00:00</td>
+        </tr>
+        <tr>
+            <td>{{ __('Thursday') }}</td>
+            <td>12:00 - 00:00</td>
+        </tr>
+        <tr>
+            <td>{{ __('Friday') }}</td>
+            <td>12:00 - 00:00</td>
+        </tr>
+        <tr>
+            <td>{{ __('Saturday') }}</td>
+            <td>12:00 - 00:00</td>
+        </tr>
+        <tr>
+            <td>{{ __('Sunday') }}</td>
+            <td>12:00 - 00:00</td>
+        </tr>
+    </table>
+</div>
